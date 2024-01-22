25d250f4b4fdaf82047335c21c5d0cc45c230df7
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-01-22 10:58:47 +0500

    Add files via upload

---
+++ class1_echo&print.php
@@ -0,0 +1,14 @@
+<h2>CLASS ONE SECTION 1 BASIC ONLY</h2>  
+<?php
+// PHP Echo Ayse Hoti Hey
+echo "Hello PHP Students";
+echo "</br>";
+// print function
+print "Hello World";
+echo "</br>";
+
+// echo Vs print function
+$fname = "Shah";  
+$lname = "Zaib";  
+echo "My name is: ".$fname,$lname;  // but in print function this is not working
+echo "</br>";
