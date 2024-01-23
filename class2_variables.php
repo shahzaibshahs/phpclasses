104138a7eeb106e7320eb54763f97cc24fd84899
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-01-23 15:55:26 +0500

    Add files via upload

---
+++ class2_variables.php
@@ -0,0 +1,11 @@
+<?php // Variables
+//Variable Hamesha Kisi underscour "_" Ya Kisi "Alphabet" Se Start Hota Hy aur isme ham numbers bi add karskty hy
+//For Example
+$_shah1 = "Php is Server Side Scripting Language";
+echo $_shah1;
+echo "</br>";
+//We can als Use varibales in + - * % etc.
+$a = 20;
+$b = 30;
+echo "Total number is ". $a+$b;
+echo "</br>";
