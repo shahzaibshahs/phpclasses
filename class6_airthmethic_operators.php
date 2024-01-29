4e0d6d2e0626491d3c269cb58df96b6e88ce8770
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-01-29 16:38:18 +0500

    Add files via upload

---
+++ class6_airthmethic_operators.php
@@ -0,0 +1,80 @@
+<?php
+
+// PHP Arithmetic Operators 
+#Example No 1 Addition
+$a = 50;
+$b = 30;
+$c = $a + $b;
+
+echo $c;
+echo "</br>";
+
+// Example No 2 Subsctraction
+
+$a = 10;
+$b = 25;
+$c = $a - $b;
+
+echo $c;
+echo "</br>";
+
+// Example No 3 Multiplication
+
+$a = 7;
+$b = 3;
+$c = $a * $b;
+
+echo $c;
+echo "</br>";
+
+// Example No 4 Division
+
+$x = 500;
+$y = 2;
+
+echo $x/$y;
+echo "</br>";
+
+// Example No 5 Modulus
+
+$s = 7;
+$f = 3;
+
+echo $s%$f; 
+echo "</br>";
+
+// Example No 6 Exponentation
+
+$a = 2;
+$b = 3;
+$c = $a ** $b;
+echo  $c; //2 ko 3 time multiply kiya gaya ha
+echo "</br>";
+
+// Example No 7 Increament opetaror
+
+$a = 10;
+$a++;
+echo $a; // har bar 1 plus hota hy Increament opetaror me
+echo "</br>";
+
+// Example No 8 decreament opetaror
+
+
+$a = 10;
+$a--;
+echo $a; // har bar 1 Minus hota hy decreament opetaror me
+echo "</br>";
+
+
+// Example No 9 Multiple calulations in one variable
+
+$a = 10;
+$b = 2;
+$c = ($a + $b) * 2/2-9;
+
+echo $c;
+echo "</br>";
+
+
+
