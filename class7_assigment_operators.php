7339fadce46bb2eca93a7d683f3f12bce603315a
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-01-30 22:13:55 +0500

    Add files via upload

---
+++ class7_assigment_operators.php
@@ -0,0 +1,62 @@
+<?php 
+// Assign
+$x = 10;
+$y = 3;
+$x = $x + $y;
+
+echo $x;
+echo "</br>";
+
+// Add then Assign	
+$x = 10;
+$y = 3;
+$x += $y;
+
+echo $x;
+echo "</br>";
+
+// Subtract then Assign
+
+$x = 10;
+$y = 3;
+$x -= $y;
+
+echo $x;
+echo "</br>";
+
+// Multiply then Assign	
+
+$x = 10;
+$y = 3;
+$x *= $y;
+
+echo $x;
+echo "</br>";
+
+// Divide then Assign
+
+$x = 10;
+$y = 3;
+$x /= $y;
+
+echo $x;
+echo "</br>";
+
+// Divide then Assign
+// (remainder)
+
+$x = 10;
+$y = 3;
+$x %= $y;
+
+echo $x;
+echo "</br>";
+
+//Exponent Values multiply multiple times
+
+$x = 10;
+$y = 3;
+$x **= $y;
+
+echo $x;
+echo "</br>";
