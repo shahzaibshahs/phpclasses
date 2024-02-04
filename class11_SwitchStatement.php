2fa1824f54b50eb7eebada68321bc7beb196f5f4
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-02-04 15:40:15 +0500

    Add files via upload

---
+++ class11_SwitchStatement.php
@@ -0,0 +1,39 @@
+<?php
+
+// PHP Switch Statement
+
+$weekday = 5;
+
+switch($weekday){
+    case 1:
+        echo "Today Is Monday";
+    break;
+
+    case 2:
+        echo "Today Is Tuesday";
+    break;
+
+    case 3:
+        echo "Today Is Wednesday";
+    break;
+
+    case 4:
+        echo "Today Is Thursday";
+    break;
+
+    case 5:
+        echo "Today Is Friday";
+    break;
+
+    case 6:
+        echo "Today Is Saturday";
+    break;
+
+    case 7:
+        echo "Today Is Sunday";
+    break;
+
+    default:
+        echo "Please Enter the Valid Day of Week";
+    
+}
