dd0f719ac5b301ddc1c22dd5d5aab4bc8d497075
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-02-06 15:38:53 +0500

    Add files via upload

---
+++ class13_StringOperator.php
@@ -0,0 +1,18 @@
+<?php
+
+// PHP String Operators
+
+$a = "Hello";
+$b = $a . " World";
+
+echo $b;
+
+echo "</br>";
+
+$s = " I am Shah";
+$s .= "zaib "; 
+$s .= "Backend  Web Developer";
+
+echo $s;
+
+?>
