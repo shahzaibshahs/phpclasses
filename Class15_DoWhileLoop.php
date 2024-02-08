5ee23b30655e8f32e053bbdf6960ea1fc040a118
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-02-08 17:59:46 +0500

    Add files via upload

---
+++ Class15_DoWhileLoop.php
@@ -0,0 +1,25 @@
+<?php
+//Do While Loop 
+// Increament
+echo "<h4>Increament In Do While Loop</h4>";
+$a = 1;
+
+do{
+    echo $a . ") Hello Php Students </br>";
+    $a++;
+}
+while($a<= 10);
+
+//Decreament
+
+echo "<h4>Decreament In Do While Loop</h4>";
+$a = 10;
+
+do{
+    echo $a . ") Hello Php Students </br>";
+    $a--;
+}
+while($a>= 1);
+
+
+?>
