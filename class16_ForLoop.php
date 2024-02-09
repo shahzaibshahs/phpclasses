3542b343fae4cde5b2dc30b3ee3584269a827a46
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-02-09 22:18:08 +0500

    Add files via upload

---
+++ class16_ForLoop.php
@@ -0,0 +1,17 @@
+<?php
+//For Loop 
+// Increament
+
+echo "<h4>Increament For While Loop</h4>";
+
+
+for($a = 1; $a <= 10; $a++){
+    echo $a . ") Hello World , This Is For Loop Example </br>";
+}
+
+echo "<h4>Increament Decreament Loop</h4>";
+
+
+for($a = 10; $a >= 1; $a--){
+    echo $a . ") Hello World , This Is For Loop Example </br>";
+}
