32cbf4ee015e0df951ffbd29f7c146cad13fefa3
Shahzaib Shah <107907599+shahzaibshahs@users.noreply.github.com>
2024-02-12 21:30:35 +0500

    Add files via upload

---
+++ class19_GotoStatement.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Goto Statement
+
+echo "This is Line no 1 <br>";
+echo "This is Line no 2 <br>";
+echo "This is Line no 3 <br>";
+goto abc;
+echo "This is Line no 4 <br>";
+echo "This is Line no 5 <br>";
+echo "This is Line no 6 <br>";
+abc:
+echo "This is Line no 7 <br>";
+echo "This is Line no 8 <br>";
